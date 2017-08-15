9d958b0b7d86bb5d0ce3ebeaebee6c692c9e2a22
VirtuV <vera.hurko@gmail.com>
2017-08-15 11:40:11 +0300

    js fixed for first page menu

--- js/firstmenu.js
+++ js/firstmenu.js
@@ -3,17 +3,29 @@ let hidden = document.querySelector('.hidden__menu');
 let close = document.querySelector('.close__icon')
 let body = document.querySelector('body');
 
-openButton.onclick = function () {
+// openButton.onclick = function () {
+//     console.log("burger icon pressed");
+//     hidden.style.display = "block";
+//     body.style.overflow = "hidden";
+// }
+
+// close.onclick = function (e) {
+//     console.log("cross icon pressed");
+//     hidden.style.display = "none";
+//     body.style.overflow = "inherit";
+//     e.stopPropagation();
+// }
+
+openButton.addEventListener('click', function() {
     console.log("burger icon pressed");
-    hidden.style.display = "block";
+    openButton.classList.add('disappear');
+    hidden.classList.add('appear');
     body.style.overflow = "hidden";
-}
-
-close.onclick = function (e) {
-    console.log("cross icon pressed");
-    hidden.style.display = "none";
+})
+close.addEventListener('click', function(e) {
+    console.log("close icon pressed");
+    hidden.classList.remove('appear');
+    openButton.classList.remove('disappear');
     body.style.overflow = "inherit";
-    e.stopPropagation();
-}
-
-
+    // e.stopPropagation();
+})
